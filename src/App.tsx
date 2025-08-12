194f3a7def7c446aa263819d0f41451f582347c6
young54 <poabc1472@naver.com>
2025-08-12 23:56:07 +0900

    (Mod) 디자인 개선

--- src/App.tsx
+++ src/App.tsx
@@ -232,8 +232,14 @@ export default function App() {
                 </select>
               </label>
             )}
-            <button onClick={resetView}>Reset View</button>
-            <button onClick={handleOpenCaptureModal}>Capture</button>
+            <button onClick={resetView}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21.5 2v6h-6M2.5 22v-6h6M2 11.5a10 10 0 0 1 18.8-4.3M22 12.5a10 10 0 0 1-18.8 4.3"/></svg>
+              Reset View
+            </button>
+            <button onClick={handleOpenCaptureModal}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path><path d="M21 4H14.82A2 2 0 0 0 13 2H8a2 2 0 0 0-1.82 2H3v16h18v-8Z"></path><circle cx="12" cy="13" r="4"></circle></svg>
+              Capture
+            </button>
           </div>
           <ViewportControls imageDimensions={imageDimensions} />
         </div>
